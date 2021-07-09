e62c4d534cd84afa7415993967fab0c0983e3b19
wonyong-park <wanyong0919@naver.com>
2021-07-09 17:42:42 +0900

    2021-07-09 관심사의 분리

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,15 +2,20 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -20,7 +25,7 @@ public class OrderServiceImpl implements OrderService {
         //할인 가격
         int discountPrice = discountPolicy.discount(member, itemPrice);
 
-        //새로운 주문 새성(멤버아이디, 아이템이름, 아이템 가격, 할인 가격)
+        //새로운 주문 생성(멤버아이디, 아이템이름, 아이템 가격, 할인 가격)
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 }
--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,11 +1,21 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    //BeforeEach는 테스트 실행전에 무조건 실행되는것
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
 
     @Test
     void join(){
