b366dd35ec70982e6d0f751d3e839ed7f21d3ffc
wonyong-park <wanyong0919@naver.com>
2021-07-12 23:55:37 +0900

    2021-07-12 싱글톤 방식의 주의점

---
+++ src/test/java/hello/core/singletone/StatefulService.java
@@ -0,0 +1,17 @@
+package hello.core.singletone;
+
+public class StatefulService {
+
+    //상태를 유지하는 필드
+//    private int price;
+
+    public int order(String name, int price) {
+        System.out.println("name = " + name + " price + " + price);
+//        this.price = price;
+        return price;
+    }
+
+    //    public int getPrice() {
+//        return price;
+//    }
+}
---
+++ src/test/java/hello/core/singletone/StatefulServiceTest.java
@@ -0,0 +1,38 @@
+package hello.core.singletone;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingletone() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        //ThreadA : A사용자가 10000원 주문
+        int userAPrice = statefulService1.order("userA", 10000);
+        //ThreadB : B사용자가 20000원 주문
+        int userBPrice = statefulService2.order("userB", 20000);
+
+        //ThreadA : A 사용자가 주문 금액 조회 -> 문제점 : 2만원으로 주문금액이 반환됨
+//        int price = statefulService1.getPrice();
+        System.out.println("price = " + userAPrice);
+
+//        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+
+    }
+
+    static class TestConfig{
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
