501ecbf5a0c9e54e710d432f391ebe416378fa30
wonyong-park <wanyong0919@naver.com>
2021-07-13 21:53:23 +0900

    2021-07-13 중복 등록과 충돌

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,23 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+
+        basePackages = "hello.core",
+        //AppConfig.java의 @Configuration이 포함되면 충돌이 나기때문에 제외(필터)
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
